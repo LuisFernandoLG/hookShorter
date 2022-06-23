34c4c54346ddf016c3b769518133c1030b164243
luis <luislopez1099lg@gmail.com>
2022-06-23 17:44:25 -0600

    input added

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,14 +1,14 @@
 import styled from 'styled-components'
 
-export const TextInput = () => {
+export const TextInput = ({handleChange, value, type}) => {
   return (
-    <TextInputStyled type='text' placeholder='Enlace . . .'></TextInputStyled>
+    <TextInputStyled type={type} value={value} onChange={handleChange} placeholder='Enlace . . .'></TextInputStyled>
   )
 }
 
 const TextInputStyled = styled.input`
-width: 500px;
-min-width: 300px;
+  width: 500px;
+  min-width: 300px;
   color: ${({ theme: { fontColor } }) => fontColor};
   font-weight: 600;
   &::placeholder {
